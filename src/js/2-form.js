ce502d07935ecf4482dfb5ad43383c3fa0453835
TiBohd <656olololol@gmail.com>
2025-09-07 21:26:03 +0300

    final-Hw9

---
+++ src/js/2-form.js
@@ -0,0 +1,40 @@
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {
+  email: '',
+  message: '',
+};
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+  try {
+    formData = JSON.parse(savedData);
+    form.email.value = formData.email || '';
+    form.message.value = formData.message || '';
+  } catch (e) {
+    console.error('Помилка парсингу даних із localStorage:', e);
+  }
+}
+
+form.addEventListener('input', event => {
+  if (event.target.name in formData) {
+    formData[event.target.name] = event.target.value.trim();
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  }
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log('Відправлено:', formData);
+
+  localStorage.removeItem(STORAGE_KEY);
+  formData = { email: '', message: '' };
+  form.reset();
+});
